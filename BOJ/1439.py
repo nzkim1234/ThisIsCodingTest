8ca2f4bdb7505564b4f10a69cad4cdd767bfad3e
누히히 <nzkim1234@naver.com>
2021-06-05 13:03:57 +0000

    Greedy 문제 풀이

--- BOJ/1439.py
+++ BOJ/1439.py
@@ -5,7 +5,7 @@ if s[0] == '0':
     count1 += 1
 else:
     count0 += 1
-for  i in range(len(s)-1):
+for i in range(len(s)-1):
     if (s[i] == '0') and (s[i] != s[i+1]):
         count0 += 1
     if (s[i] == '1') and (s[i] != s[i+1]):
