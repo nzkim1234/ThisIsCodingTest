f4cdd28a1cea54b9c2fdac72bc20ac8fc613e63d
누히히 <nzkim1234@naver.com>
2021-06-06 08:07:02 +0000

    Greedy  문제 풀이

--- BOJ/1449.py
+++ BOJ/1449.py
@@ -1,22 +1,10 @@
 n, l = map(int, input().split())
-l -= 1
-pipe = []
-count = 0
-for i in range(1001):
-    pipe.append(0)
+count = 1
 location = list(map(int, input().split()))
 location.sort()
+pipe = location[0] + l - 1
 for i in location:
-    pipe[i] = 1
-for i in range(location[len(location)-1]+1):
-    if i >= 1001:
-        break
-    if pipe[i] == 1:
-        pipe[i] = 0
-        for j in range(l+1):
-            if i >= 1000:
-                break
-            pipe[i+j] = 0
+    if i > pipe:
+        pipe = i + l - 1
         count += 1
 print(count)
- 
