a574c64ea89162fce404bdf49319c6de28292481
누히히 <nzkim1234@naver.com>
2021-06-16 13:16:30 +0000

    DFS 문제 풀이

--- BOJ/1068.py
+++ BOJ/1068.py
@@ -1,7 +1,7 @@
 def dfs(v):
     l = graph[v]
     graph[v] = -2
-    if l != -2:    
+    if l != -2:
         for i in l:
             if v in graph[i]:
                 break
@@ -9,12 +9,10 @@ def dfs(v):
                 dfs(i)
 
 
-
 n = int(input())
 graph = [[] for _ in range(n)]
 node = list(map(int, input().split()))
 for i in range(n):
-    
     if node[i] == -1:
         graph[i].append(-1)
     else:
