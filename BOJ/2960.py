565bad3bbaf57fd432a2a7f7be3d77fc580d6168
BoYoon <54830745+nzkim1234@users.noreply.github.com>
2021-10-12 23:16:29 +0900

    문제 풀이

--- BOJ/2960.py
+++ BOJ/2960.py
@@ -3,16 +3,15 @@ n_l = [True for _ in range(n+1)]
 n_l[0] = False
 n_l[1] = False
 result = 0
-while k > 0:
-    for i in range(n+1):
-        if n_l[i] == True:
-            for j in range(i, n+1, i):
-                if n_l[j] == True:
-                    k -= 1
-                    n_l[j] = False
-                    if k == 0:
-                        result = j
-                        break
-        if k == 0:
-            break
+for i in range(n+1):
+    if n_l[i] == True:
+        for j in range(i, n+1, i):
+            if n_l[j] == True:
+                k -= 1
+                n_l[j] = False
+                if k == 0:
+                    result = j
+                    break
+    if k == 0:
+        break
 print(result)
