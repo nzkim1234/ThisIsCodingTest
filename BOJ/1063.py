0b883d42101279064f10111c814e6a2aa2bad72c
boyoon <nzkim1234@naver.com>
2021-12-06 19:10:48 +0900

    문제 풀이

--- BOJ/1063.py
+++ BOJ/1063.py
@@ -1,11 +1,11 @@
-from sys import getrecursionlimit, stdin
+from sys import stdin
 
 king, stone, n = stdin.readline().split()
 king_x, king_y = king
 stone_x, stone_y = stone
 chess_board = [[0 for _ in range(8)] for _ in range(8)]
-king_loc = [int(king_y) - 1, ord(king_x) - 65]
-stone_loc = [int(stone_y) - 1, ord(stone_x) - 65]
+king_loc = [int(king_y) - 1, ord(king_x) - 65]  # 문자를 숫자로 변환
+stone_loc = [int(stone_y) - 1, ord(stone_x) - 65]  # 문자를 숫자로 변환
 move_loc = []
 movement = []
 
@@ -16,6 +16,7 @@ for move in movement:
     new_king_loc = []
     new_stone_loc = []
 
+    # 이동방향에 따라 이동
     if move == 'R':
         move_loc = [0, 1]
     elif move == 'L':
@@ -33,19 +34,19 @@ for move in movement:
     elif move == 'LB':
         move_loc = [-1, -1]
         
-    new_king_loc = [king_loc[0] + move_loc[0], king_loc[1] + move_loc[1]]
+    new_king_loc = [king_loc[0] + move_loc[0], king_loc[1] + move_loc[1]]  # 이동한 새로운 킹의 좌표
 
-    if 0 <= new_king_loc[0] < 8 and 0 <= new_king_loc[1] < 8:
-        if new_king_loc[0] == stone_loc[0] and new_king_loc[1] == stone_loc[1]:
-            new_stone_loc = [stone_loc[0] + move_loc[0], stone_loc[1] + move_loc[1]]
+    if 0 <= new_king_loc[0] < 8 and 0 <= new_king_loc[1] < 8:  # 새로운 킹이 체스판 내부일 때
+        if new_king_loc[0] == stone_loc[0] and new_king_loc[1] == stone_loc[1]:  # 새로운 킹의 좌표가 돌과 같을 때
+            new_stone_loc = [stone_loc[0] + move_loc[0], stone_loc[1] + move_loc[1]]  # 돌을 이동
             
-            if 0 <= new_stone_loc[0] < 8 and 0 <= new_stone_loc[1] < 8:
+            if 0 <= new_stone_loc[0] < 8 and 0 <= new_stone_loc[1] < 8: # 이동한 새로운 좌표가 체스판 내부이면 킹, 돌의 좌표 저장
                 king_loc = new_king_loc
                 stone_loc = new_stone_loc
-        else:
-            king_loc = new_king_loc
+        else:  # 새로운 킹의 좌표가 돌과 같지 않을 때
+            king_loc = new_king_loc  # 좌표 저장
     
-king = chr(king_loc[1] + 65) + str(king_loc[0] + 1)
-stone = chr(stone_loc[1] + 65) + str(stone_loc[0] + 1)
+king = chr(king_loc[1] + 65) + str(king_loc[0] + 1)  # 숫자를 문자로 변환
+stone = chr(stone_loc[1] + 65) + str(stone_loc[0] + 1)  # 숫자를 문자로 변환
 print(king)
 print(stone)
--- BOJ/3190.py
+++ BOJ/3190.py
@@ -21,12 +21,12 @@ for l in range(int(stdin.readline())):
     switch_time, rotate = stdin.readline().split()
     movement.append([int(switch_time), rotate])
 
-switch_time, change_direction = movement.popleft()
+switch_time, rotate = movement.popleft()  # 방향이 바뀌는 시간, 바뀌는 방향 저장
 
 while True:
-
     if switch_time == time:
-        if change_direction == 'D':
+        #  rortate 값에 따라 진행 방향을 바꾼다.
+        if rotate == 'D':
             direction_index += 1
             
             if direction_index > 3:
@@ -36,24 +36,23 @@ while True:
             
             if direction_index < 0:
                 direction_index = 3
+        
+        if movement:  # 덱이 남아있을 시 switch_time, rotate 값을 변경
+            switch_time, rotate = movement.popleft()
 
-        if movement:
-            switch_time, change_direction = movement.popleft()
+    n_x = snake_head[0] + direction[direction_index][0]  # 새로운 뱀 머리의 x좌표
+    n_y = snake_head[1] + direction[direction_index][1]  # 새로운 뱀 머리의 y좌표
 
-    x, y = snake_head
-    n_x = x + direction[direction_index][0]
-    n_y = y + direction[direction_index][1]
-
-    if 0 <= n_x < n and 0 <= n_y < n and graph[n_x][n_y] != 1:
-        snake_body.append(snake_head)
-        snake_head = [n_x, n_y]
-        if graph[n_x][n_y] != 2:
-            r_x, r_y = snake_body.popleft()
-            graph[r_x][r_y] = 0
-        graph[n_x][n_y] = 1
+    if 0 <= n_x < n and 0 <= n_y < n and graph[n_x][n_y] != 1:  # 새로운 뱀의 좌표가 그래프 내부이며 값이 1(뱀의 몸통) 이 아닐 때
+        snake_body.append(snake_head)  # 이전 뱀 머리의 좌표를 몸통에 저장
+        snake_head = [n_x, n_y]  # 새로운 뱀 머리의 좌표를 주장
+        if graph[n_x][n_y] != 2:  # 새로운 뱀 머리의 좌표값이 2가 아니면
+            r_x, r_y = snake_body.popleft()  # 뱀 몸통의 마지막을 삭제
+            graph[r_x][r_y] = 0  # 마지막 뱀 몸통의 값을 0 으로 변경
+        graph[n_x][n_y] = 1  # 새로운 뱀 머리의 좌표값을 1로 변경
     else:
         break
-    
+
     time += 1
 
 print(time + 1)
